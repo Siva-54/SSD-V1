8a4d83082313c90ffed7e9167bd5fc16629fa871
siva <siva.anubothula@gmail.com>
2025-04-17 09:47:33 +0530

    Initialized repo

---
+++ client/src/components/Credits.jsx
@@ -0,0 +1,9 @@
+import { Github } from "../assets/icons";
+
+export default function Credits() {
+  return (
+    <section className="credits">
+      <p>Created by Group 12 SE lab</p>
+    </section>
+  );
+}
